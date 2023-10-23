803e04fcb4250612dda58a217177db3cf97d751d
Dawid Bobowski <dbobowski3@gmail.com>
2023-10-23 22:26:16 +0200

    FE: hide mobile menu on button or click outside

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,61 @@
 import { Link, useNavigate } from 'react-router-dom';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { useAuth } from '../auth/useAuth';
+
+type BootstrapWindow = {
+  bootstrap: {
+    Collapse: new (element: HTMLElement, options?: { toggle: boolean }) => BootstrapCollapse;
+  };
+} & typeof window;
+
+interface BootstrapCollapse {
+  hide: () => void;
+}
 
 const Header: React.FC = () => {
   const { currentUser, logout } = useAuth();
   const navigate = useNavigate();
 
+  const navbarMenuRef = useRef<HTMLDivElement>(null);
+  const toggleButtonRef = useRef<HTMLButtonElement>(null);
+
   const handleLoginClick = () => {
+    toggleMobileMenu();
     navigate('/login');
   };
 
   const handleLogoutClick = () => {
+    toggleMobileMenu();
     logout();
     navigate('/');
   };
+
+  const toggleMobileMenu = () => {
+    const bsWindow = window as unknown as BootstrapWindow;
+    const bsCollapse = new bsWindow.bootstrap.Collapse(navbarMenuRef.current!, {
+      toggle: false
+    });
+    bsCollapse.hide();
+  }
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (navbarMenuRef.current && !navbarMenuRef.current.contains(event.target as Node) &&
+        toggleButtonRef.current && !toggleButtonRef.current.contains(event.target as Node)) {
+      const bsWindow = window as unknown as BootstrapWindow;
+      const bsCollapse = new bsWindow.bootstrap.Collapse(navbarMenuRef.current, {
+        toggle: false
+      });
+      bsCollapse.hide();
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
@@ -22,10 +63,10 @@ const Header: React.FC = () => {
         <Link className='navbar-brand' to='/'>
           GAMR8
         </Link>
-        <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+        <button ref={toggleButtonRef} className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
           <span className='navbar-toggler-icon'></span>
         </button>
-        <div className='collapse navbar-collapse justify-content-end' id='navbarNav'>
+        <div ref={navbarMenuRef} className='collapse navbar-collapse justify-content-end' id='navbarNav'>
           <ul className='navbar-nav mr-auto'>
             <li className='nav-item'>
               <a className='nav-link' href='#'>Home</a>
