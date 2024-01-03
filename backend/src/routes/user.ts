8f3a2f8d7aadac043809ec6666aa98e75c1a0b43
Dawid Bobowski <dbobowski3@gmail.com>
2024-01-03 21:55:30 +0100

    BE: add /api/user/:username endpoint

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, User } from '@prisma/client';
 
 const router = Router();
 const prisma = new PrismaClient();
@@ -10,7 +10,37 @@ router.get('/api/users', async (req, res) => {
     res.json(users);
   } catch (error) {
     console.error('Error fetching users:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({
+      error: 'Internal Server Error: unable to retrieve users from the database',
+    });
+  }
+});
+
+router.get('/api/user/:username', async (req, res) => {
+  try {
+    const username: string = req.params.username;
+    const user: User | null = await prisma.user.findFirst({
+      where: {
+        username,
+      },
+    });
+    if (user) {
+      return res.status(200).json({
+        success: true,
+        message: 'User found',
+        user,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: 'User not found',
+      user: null,
+    });
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    res.status(500).json({
+      error: 'Internal Server Error: unable to retrieve user info from the database',
+    });
   }
 });
 
