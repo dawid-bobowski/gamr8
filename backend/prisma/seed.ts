974c937b5bff285f7dc5ddbac47364ea0b851cb5
Dawid Bobowski <dbobowski3@gmail.com>
2024-01-03 22:44:34 +0100

    DB: update review schema and database scripts

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -97,6 +97,67 @@ const games: PrismaGame[] = [
   },
 ];
 
+interface PrismaReview {
+  author_username: string;
+  game_id: number;
+  title: string;
+  description: string;
+  rating: number;
+  date_posted: string;
+}
+
+const reviews: PrismaReview[] = [
+  {
+    author_username: 'dave',
+    game_id: 2,
+    title: 'A Disappointing Experience in an Overhyped World',
+    description: 'As an avid gamer, I was incredibly excited to delve into "Elden Ring," a game surrounded by immense \
+      hype and high expectations. Unfortunately, my experience was far from what I anticipated.\n\nFirstly, the \
+      game\'s open-world, while vast and visually appealing, felt overwhelmingly directionless. I spent hours \
+      wandering aimlessly, often finding myself lost in its enormity without a clear sense of purpose or progress. \
+      The lack of guidance and coherent storytelling left me more frustrated than entertained.\n\nCombat, a core \
+      aspect of the game, was another letdown. It felt unpolished and unfairly punishing, with enemy AI that was \
+      often glitchy and unpredictable. The balance between challenge and fun was missing, turning what should have \
+      been exhilarating battles into tedious and frustrating encounters.\n\nMoreover, the performance issues were \
+      hard to overlook. I encountered frequent frame rate drops and long loading times that significantly disrupted \
+      the gameplay experience. For a game of this caliber and coming from a renowned developer, these technical \
+      shortcomings were surprising and unacceptable.\n\nIn conclusion, "Elden Ring" was a letdown. It may offer an \
+      expansive world to explore, but the lack of direction, flawed combat, and technical issues significantly marred \
+      my experience. It\'s a game that, despite its potential, failed to live up to the hype for me.',
+    rating: 3,
+    date_posted: new Date('2023-01-01').toISOString(),
+  },
+  {
+    author_username: 'xardas',
+    game_id: 1,
+    title: 'A Time-Bending Thrill Ride: My Take on Deathloop',
+    description: 'Deathloop delivers an exhilarating experience that blends innovative gameplay with a compelling \
+      narrative. The time-loop mechanic isn\'t just a gimmick; it\'s seamlessly integrated into both the story and \
+      the gameplay, offering a refreshing challenge. The art style is vibrant and unique, perfectly complementing the \
+      mysterious island of Blackreef. While the game occasionally falters with some repetitive elements, its strong \
+      voice acting and intriguing plot twists more than make up for it. Deathloop is a must-play for those seeking a \
+      game that breaks the mold in the best way possible.',
+    rating: 8,
+    date_posted: new Date('2023-01-02').toISOString(),
+  },
+  {
+    author_username: 'dave',
+    game_id: 3,
+    title: 'Epic Adventures in Azeroth: My Journey Through World of Warcraft',
+    description: 'World of Warcraft is an iconic staple in the MMORPG genre, offering an expansive world filled with \
+      lore, diverse races, and countless quests. The game\'s ability to constantly evolve through expansions keeps the \
+      content fresh and engaging. Its rich, detailed world brimming with history and culture makes every quest feel \
+      like part of a grander narrative. The social aspect of forming parties and guilds adds a delightful layer of \
+      community interaction that enhances the overall experience.\n\nThe game\'s graphics, though not cutting-edge, \
+      have a timeless charm that contributes to its unique aesthetic. Combat mechanics are well-designed, offering \
+      depth and variety to suit different playstyles. While the subscription model might be a deterrent for some, the \
+      quality and quantity of content available provide excellent value. World of Warcraft remains a benchmark in the \
+      genre, captivating veterans and newcomers alike with its enchanting world and dynamic gameplay.',
+    rating: 8,
+    date_posted: new Date('2023-01-03').toISOString(),
+  },
+];
+
 async function main() {
   // Insert users
   for (const user of users) {
@@ -112,6 +173,14 @@ async function main() {
       data: game,
     });
     console.log('Inserted game:', result);
+  }
+
+  // Insert reviews
+  for (const review of reviews) {
+    const result = await prisma.review.create({
+      data: review,
+    });
+    console.log('Inserted review:', result);
   }
 }
 
--- backend/prisma/truncate.ts
+++ backend/prisma/truncate.ts
@@ -3,9 +3,14 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function clearDatabase() {
+  await prisma.review.deleteMany({});
   await prisma.user.deleteMany({});
   await prisma.game.deleteMany({});
-  await prisma.review.deleteMany({});
+
+  await prisma.$executeRaw`ALTER TABLE Review AUTO_INCREMENT = 1;`;
+  await prisma.$executeRaw`ALTER TABLE User AUTO_INCREMENT = 1;`;
+  await prisma.$executeRaw`ALTER TABLE Game AUTO_INCREMENT = 1;`;
+
 
   console.log('Database cleared');
 }
